072ae2de28907b182b43c8078abe3d28d59e3004
kintneda <kintneda@rose-hulman.edu>
2018-01-17 08:22:22 -0500

    Finished ToDo's

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -131,13 +131,14 @@ def MUTATE_delete_negatives(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    n = len(numbers)
-    for k in range(n):
+
+
+    for k in range(len(numbers)-1,-1,-1):
         if numbers[k] < 0:
-            numbers.__delitem__(k)
+            del numbers[k]
 
 
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # Done: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
